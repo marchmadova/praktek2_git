8f32a67a8c88edc84944c25a9095d8b70495af3d
march madova jovanovic <marchmadova@telkomuniversity.ac.id>
2023-03-10 10:57:41 +0700

    commit v1.2

--- Tugas_commit_remote_repo.py
+++ Tugas_commit_remote_repo.py
@@ -15,3 +15,22 @@ user = {
 #Dalam program yang sama, buatlah program untuk menerima input dari user (commit v1.1)
 nama = input("Enter nama:")
 print("Username is: " + nama)
+
+
+#Dalam program yang sama, buatlah program untuk melakukan pencarian ke dalam 
+#dictionary yang sudah ditulis. Lakukan pencarian berdasarkan input yang diterima pada nomor (2) 
+#Lakukan pencarian berdasarkan atribut nama. Jika nama yang dicari ada dalam dictionary, tampilkan 
+#data lengkapnya dari nama hingga tanggal lahir. Jika tidak ditemukan nama yang dicari, 
+#tampilkan “data tidak ada” (commit v1.2)
+
+
+
+user = [{"nama": 'arsene lupin', "nik": 1234, "jenis_kelamin": "male", "tanggal_lahir":"1902-03-23"},
+{"nama": "sherlock holmes", "nik": 4321, "jenis_kelamin": "male", "tanggal_lahir":"1876-08-16"},
+{"nama": "irene adler", "nik":6789, "jenis_kelamin": "female", "tanggal_lahir":"1884-10-07"},]
+
+if 
+nama = arsene lupin
+print ("nama": 'arsene lupin', "nik": 1234, "jenis_kelamin": "male", "tanggal_lahir":"1902-03-23")
+
+if 
